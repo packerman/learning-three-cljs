474b69d65014c9b63e3dc46d8b5b642ca8ac7c50
Piotr Ackermann <pioacker@gmail.com>
2016-10-30 12:17:19 +0100

    light, materials and shadows

--- src/cljs/learning_three_cljs/core.cljs
+++ src/cljs/learning_three_cljs/core.cljs
@@ -3,52 +3,58 @@
 (enable-console-print!)
 
 (defn init []
-      (letfn [(make-renderer []
-                    (doto (js/THREE.WebGLRenderer.)
-                          (.setClearColor 0xEEEEEE)
-                          (.setSize (.-innerWidth js/window) (.-innerHeight js/window))))
-              (make-plane []
-                (doto (js/THREE.Mesh.
-                        (js/THREE.PlaneGeometry. 60 20 1 1)
-                        (js/THREE.MeshBasicMaterial. (js-obj "color" 0xcccccc)))
-                  (-> .-rotation (.set (* -0.5 js/Math.PI) 0 0))
-                  (-> .-position (.set 15 0 0))))
-              (make-cube []
-                (doto (js/THREE.Mesh.
-                        (js/THREE.BoxGeometry. 4 4 4)
-                        (js/THREE.MeshBasicMaterial. (js-obj "color" 0xff0000
-                                                             "wireframe" true)))
-                    (-> .-position (.set -4 3 0))))
-              (make-sphere []
-                (doto (js/THREE.Mesh.
-                        (js/THREE.SphereGeometry. 4 20 20)
-                        (js/THREE.MeshBasicMaterial. (js-obj "color" 0x7777ff
-                                                             "wireframe" true)))
-                    (-> .-position (.set 20 4 2))))
-              (make-camera []
-                  (doto (js/THREE.PerspectiveCamera. 45 (/ (.-innerWidth js/window)
-                                                           (.-innerHeight js/window))
-                                                  0.1
-                                                  1000)
-                    (-> .-position (.set -30 40 30))))]
-            (let [scene (js/THREE.Scene.)
-                  camera (make-camera)
-                  renderer (make-renderer)
-                  axes (js/THREE.AxisHelper. 20)
-                  plane (make-plane)
-                  cube (make-cube)
-                  sphere (make-sphere)]
-              (.lookAt camera (.-position scene))
-              (.add scene
-                    axes
-                    plane
-                    cube
-                    sphere
-                    camera)
-              (-> js/document
-                  (.getElementById "app")
-                  (.appendChild (.-domElement renderer)))
-              (.render renderer scene camera))))
+  (letfn [(make-renderer []
+            (doto (js/THREE.WebGLRenderer.)
+              (.setClearColor (js/THREE.Color. 0xEEEEEE))
+              (.setSize (.-innerWidth js/window) (.-innerHeight js/window))
+              (-> .-shadowMap .-enabled (set! true))))
+          (make-plane []
+                      (doto (js/THREE.Mesh.
+                             (js/THREE.PlaneGeometry. 60 20 1 1)
+                             (js/THREE.MeshLambertMaterial. (js-obj "color" 0xffffff)))
+                        (-> .-rotation (.set (* -0.5 js/Math.PI) 0 0))
+                        (-> .-position (.set 15 0 0))
+                        (-> .-receiveShadow (set! true))))
+          (make-cube []
+                     (doto (js/THREE.Mesh.
+                            (js/THREE.BoxGeometry. 4 4 4)
+                            (js/THREE.MeshLambertMaterial. (js-obj "color" 0xff0000)))
+                        (-> .-position (.set -4 3 0))
+                        (-> .-castShadow (set! true))))
+          (make-sphere []
+                       (doto (js/THREE.Mesh.
+                              (js/THREE.SphereGeometry. 4 20 20)
+                              (js/THREE.MeshLambertMaterial. (js-obj "color" 0x7777ff)))
+                         (-> .-position (.set 20 4 2))
+                         (-> .-castShadow (set! true))))
+          (make-camera []
+                       (doto (js/THREE.PerspectiveCamera. 45 (/ (.-innerWidth js/window)
+                                                                (.-innerHeight js/window))
+                                                          0.1
+                                                          1000)
+                         (-> .-position (.set -30 40 30))))
+          (make-spot-light []
+                           (doto (js/THREE.SpotLight. 0xffffff)
+                             (-> .-position (.set -40 60 -10))
+                             (-> .-castShadow (set! true))))]
+    (let [scene (js/THREE.Scene.)
+          camera (make-camera)
+          renderer (make-renderer)
+          axes (js/THREE.AxisHelper. 20)
+          plane (make-plane)
+          cube (make-cube)
+          sphere (make-sphere)
+          spot-light (make-spot-light)]
+      (.lookAt camera (.-position scene))
+      (.add scene
+            axes
+            plane cube sphere
+            camera
+            spot-light)
+      (-> js/document
+          (.getElementById "app")
+          (.appendChild (.-domElement renderer)))
+      (.render renderer scene camera))))
 
 (set! (.-onload js/window) init)
 
